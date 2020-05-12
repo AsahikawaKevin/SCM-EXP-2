764ccf4a86ae4ee87589a221f6721b6ca761ece0
SicongLiu998 <1403955743@qq.com>
2020-05-12 15:17:12 +0800

    Merge branch 'master' of https://github.com/AsahikawaKevin/SCM-EXP-2

---
+++ Main.java
@@ -0,0 +1,19 @@
+package list;
+
+public class Main {
+	public static  void main(String[] arg){
+		LinkedList<Integer> linkedList= new LinkedList<>();
+		for(int i=0;i<5;i++){
+			linkedList.addFirst(i);
+			System.out.println(linkedList);
+		}
+		linkedList.add(2, 555);
+		System.out.println(linkedList);
+		linkedList.remove(2);
+		System.out.println(linkedList);
+		linkedList.removeFirst();
+		System.out.println(linkedList);
+		linkedList.removeLast();
+		System.out.println(linkedList);
+	}
+}
---
+++ Queue.java
@@ -0,0 +1,9 @@
+package list;
+
+public interface Queue<E> {
+	int getSize();
+	boolean isEmpty();
+	E dequeue();
+	E getFront();
+	void enqueue(E e);
+}
